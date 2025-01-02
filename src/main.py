e3dcae22c6dcf7ed347e45695d0297f0d33c40a9
AdelKazzaz <adelazizalkzaz@gmail.com>
2025-01-02 23:59:30 +0200

    refactor: update FastAPI lifespan management for database connection handling

--- src/main.py
+++ src/main.py
@@ -2,19 +2,19 @@ from fastapi import FastAPI
 from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
+from contextlib import asynccontextmanager
 
+app = FastAPI()
 
-app=FastAPI()
-
-
-@app.lifespan("startup")
-async def startup_db_client():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     settings = get_settings()
     app.mongo_connection = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_connection[settings.MONGODB_DATABASE]
-@app.lifespan("shutdown")
-async def startup_db_client():
+    yield
     app.mongo_connection.close()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
