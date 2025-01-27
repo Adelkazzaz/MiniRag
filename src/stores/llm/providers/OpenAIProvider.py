42ff9a9bc366bce72c29c72d170677d361e69261
AdelKazzaz <adelazizalkzaz@gmail.com>
2025-01-27 15:15:21 +0200

    feat: implement constructor for OpenAIProvider with configurable parameters and logging

--- src/stores/llm/providers/OpenAIProvider.py
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -4,4 +4,18 @@ from openai import OpenAI
 import logging
 
 class OpenAIProvider(LLMInterface):
-    pass
+    
+    def __init__(self, api_key: str,
+                       api_url: str = None,
+                       default_input_max_charcters: int = 500,
+                       default_generation_max_tokens: int = 1000,
+                       default_temperature: float = 0.2,):
+        
+        self.api_key = api_key
+        self.client = OpenAI(api_key)
+        self.api_url = api_url
+        self.default_input_max_charcters = default_input_max_charcters
+        self.default_generation_max_tokens = default_generation_max_tokens
+        self.default_temperature = default_temperature
+        self.logger = logging.getLogger(__name__)
+        
